478da565d792c34d029c88b086c47612c6b5c2af
oslead-solutions <oumou.vero.sow@gmail.com>
2025-07-31 21:38:19 +0000

    Integration endpoint authentification

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth/auth.service';
+import { AuthService, LoginRequest } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -12,8 +12,10 @@ export class LoginPageComponent {
   password = '';
   errorMessage: string = '';
   errorField: string = '';
+  error: string = '';
+  loading: boolean = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) { }
 
   onLogin(): void {
     // Réinitialiser les messages d'erreur
@@ -34,17 +36,51 @@ export class LoginPageComponent {
     }
 
     // Appeler le service d'authentification
-    if (this.authService.login(this.username, this.password)) {
-      const role = this.authService.getUser().role;
-      if (role === 'directeur') {
-        this.router.navigate([`/dashboard`]);
-      } else {
-        this.router.navigate([`/courses`]);
+    /* if (this.authService.login(this.username, this.password)) {
+       const role = this.authService.getUser().role;
+       if (role === 'directeur') {
+         this.router.navigate([`/dashboard`]);
+       } else {
+         this.router.navigate([`/courses`]);
+       }
+     } else {
+       this.errorMessage = 'Identifiant ou mot de passe incorrect.';
+       this.errorField = 'global';
+     }*/
+    const loginData: LoginRequest = {
+      email: this.username,
+      password: this.password
+    };
+    this.loading = true;
+    this.authService.login(loginData).subscribe({
+      next: (res) => {
+        let role = res.role;
+        if (res.role === 'DIRECTEUR' || res.role === 'SUPER_ADMIN') {
+          role = "directeur"
+        } else if (res.role === 'RESPONSABLE_ETUDES') {
+          role = "responsable"
+        }
+        res.role = role;
+        res.username = res.email;
+
+        this.authService.saveConnectedUser(res);
+        console.log('Login success:', res);
+        this.loading = false;
+        if (role === 'directeur') {
+          this.router.navigate([`/dashboard`]);
+        } else {
+          this.router.navigate([`/courses`]);
+        }
+        // redirection ou autre action
+      },
+      error: (err) => {
+        console.error('Login failed:', err);
+        this.errorMessage = err.error.error || 'Echec de connexion.';
+        this.error = err.error.error || 'Echec de connexion.';
+        this.loading = false;
       }
-    } else {
-      this.errorMessage = 'Identifiant ou mot de passe incorrect.';
-      this.errorField = 'global';
-    }
+    });
+
   }
 
   /**
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,67 +1,72 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators'
 import { Router } from '@angular/router';
+export interface LoginRequest {
+    email: string;
+    password: string;
+}
 
+export interface LoginResponse {
+    id: number;
+    role: string;
+    email: string;
+    token: string;
+    username?: string;
+}
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root'
 })
 export class AuthService {
-  private authenticated = false;
-  private users = [
-    { username: 'Patrick', password: 'AE!rkN$ba3y6zoS!', role: 'directeur' },
-    { username: 'Sophie', password: '123456', role: 'responsable' },
-  ];
 
-  constructor(private router: Router) {}
+    /*private authenticated = false;
+    private users = [
+      { username: 'Patrick', password: 'AE!rkN$ba3y6zoS!', role: 'directeur' },
+      { username: 'Sophie', password: '123456', role: 'responsable' },
+    ];*/
 
-  /**
-   * Authentifie l'utilisateur
-   * @param username
-   * @param password
-   * @returns true si l'utilisateur est authentifié, false sinon
-   */
-  login(username: string, password: string): boolean {
-    const user = this.users.find(
-      (u) => u.username === username && u.password === password
-    );
+    private authenticated = false;
+    private baseUrl = 'http://localhost:8081/api/auth';
 
-    if (user) {
-      // Enregistrer l'utilisateur authentifié dans le localStorage
-      localStorage.setItem('user', JSON.stringify(user));
-      return true;
+
+    constructor(private router: Router, private http: HttpClient) { }
+
+    login(data: LoginRequest): Observable<LoginResponse> {
+        return this.http.post<LoginResponse>(`${this.baseUrl}/login`, data);
+    }
+    saveConnectedUser(user: LoginResponse): void {
+        localStorage.setItem('user', JSON.stringify(user));
+    }
+    // Vérifie si un utilisateur est authentifié
+    checkAuthentication() {
+        const user = localStorage.getItem('user');
+        this.authenticated = !!user;
+
+        if (!this.authenticated) {
+            this.router.navigate(['/login-page']);
+        }
     }
 
-    return false;
-  }
-
-  // Vérifie si un utilisateur est authentifié
-  checkAuthentication() {
-    const user = localStorage.getItem('user');
-    this.authenticated = !!user;
-
-    if (!this.authenticated) {
-      this.router.navigate(['/login-page']);
+    isAuthenticated(): boolean {
+        const user = localStorage.getItem('user');
+        this.authenticated = !!user;
+        return this.authenticated;
     }
-  }
 
-  isAuthenticated(): boolean {
-    const user = localStorage.getItem('user');
-    this.authenticated = !!user;
-    return this.authenticated;
-  }
+    /**
+     * Récupère l'utilisateur actuellement connecté
+     * @returns L'utilisateur ou null s'il n'est pas connecté
+     */
+    getUser(): any {
+        const user = localStorage.getItem('user');
+        return user ? JSON.parse(user) : null;
+    }
 
-  /**
-   * Récupère l'utilisateur actuellement connecté
-   * @returns L'utilisateur ou null s'il n'est pas connecté
-   */
-  getUser(): any {
-    const user = localStorage.getItem('user');
-    return user ? JSON.parse(user) : null;
-  }
-
-  /**
-   * Déconnecte l'utilisateur
-   */
-  logout(): void {
-    localStorage.removeItem('user');
-  }
+    /**
+     * Déconnecte l'utilisateur
+     */
+    logout(): void {
+        localStorage.removeItem('user');
+    }
 }
